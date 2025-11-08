e71514d38e271bccc8ead324143234c8ae67f4ee
Man <nguyenphanminhman04@gmail.com>
2025-11-08 15:10:09 +0700

    fix DotenConfig

--- backend/src/main/java/modules/config/DotenvConfig.java
+++ backend/src/main/java/modules/config/DotenvConfig.java
@@ -5,8 +5,19 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DotenvConfig {
+//    static {
+//        Dotenv dotenv = Dotenv.load();
+//        System.setProperty("GEMINI_KEY", dotenv.get("GEMINI_KEY"))cd ..;
+//    }
     static {
-        Dotenv dotenv = Dotenv.load();
-        System.setProperty("GEMINI_KEY", dotenv.get("GEMINI_KEY"));
-    }
+        Dotenv dotenv = Dotenv.configure()
+                .directory("./")
+                .filename(".env")
+                .ignoreIfMissing()
+                .load();
+
+        if (dotenv.get("GEMINI_KEY") != null) {
+            System.setProperty("GEMINI_KEY", dotenv.get("GEMINI_KEY"));
+        }
+}
 }
